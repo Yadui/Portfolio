4429c8a077d2114fd89753a0b4427f521d68c60b
yadui-foetron <abhinav.yadav@foetron.com>
2025-12-11 12:24:17 +0530

    Remove Header and StairTransition from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,7 @@ import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
 // Components
-import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
-import StairTransition from "@/components/StairTransition";
 
 const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
@@ -21,8 +19,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={jetbrainsMono.variable}>
-        <Header />
-        <StairTransition />
         <PageTransition>
           {children}
         </PageTransition>
